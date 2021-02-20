125dfd571c163297546a2972736427c98809a29a
Ashley Pittman <ashley@pittman.co.uk>
2021-02-20 09:18:39 +0000

    Run FI without dfuse.
    Signed-off-by: Ashley Pittman <ashley@pittman.co.uk>

--- utils/run_in_ga.sh
+++ utils/run_in_ga.sh
@@ -3,14 +3,24 @@
 set -e
 
 echo ::group::Build
-scons --jobs 10 install PREFIX=/opt/daos COMPILER=clang TARGET_TYPE=debug BUILD_TYPE=dev
+scons --jobs 10 PREFIX=/opt/daos COMPILER=clang TARGET_TYPE=debug BUILD_TYPE=dev
 echo ::endgroup::
 
+cat daos.conf
+
+echo ::group::Install
+scons --jobs 10 install
+echo ::endgroup::
+
+echo ::group::Setting up daos_admin
 . utils/sl/setup_local.sh
 ./utils/setup_daos_admin.sh
+echo ::endgroup::
 
 echo ::group::Key Value test
 ./utils/node_local_test.py --no-root kv
 echo ::endgroup::
-#./utils/node_local_test.py --no-root fi
 
+echo ::group::Fault injection test
+./utils/node_local_test.py --no-root fi
+echo ::endgroup::
