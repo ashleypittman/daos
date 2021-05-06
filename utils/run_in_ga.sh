8439e243d3c896c7957b6f1ac5b059ed869837e8
Ashley Pittman <ashley.m.pittman@intel.com>
2021-05-06 11:57:51 +0100

    Retry.
    Use NLT junit results.
    Run the new cont_copy test.
    Use a debug build.
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/run_in_ga.sh
+++ utils/run_in_ga.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 set -e
-ls -l
 
 # BUILD_TYPE=debug does not work
 
 echo ::group::Build
-scons --jobs 10 PREFIX=/opt/daos COMPILER=clang TARGET_TYPE=release BUILD_TYPE=dev
+scons --jobs 10 PREFIX=/opt/daos COMPILER=clang TARGET_TYPE=release BUILD_TYPE=debug
 echo ::endgroup::
 
 cat daos.conf
@@ -20,8 +19,12 @@ echo ::group::Setting up daos_admin
 ./utils/setup_daos_admin.sh
 echo ::endgroup::
 
-echo ::group::Key Value test
+echo ::group::Container copy test
 export LD_LIBRARY_PATH=/opt/daos/lib:/opt/daos/lib64:/opt/daos/bin/../lib64/daos_srv/../../prereq/release/spdk/lib/:$LD_LIBRARY_PATH
+./utils/node_local_test.py --no-root --test cont_copy
+echo ::endgroup::
+
+echo ::group::Key Value test
 ./utils/node_local_test.py --no-root kv
 echo ::endgroup::
 
--- utils/run_in_ga_wrapper.sh
+++ utils/run_in_ga_wrapper.sh
@@ -4,4 +4,4 @@ docker images -a --filter label=DAOS=true
 
 IMAGE=$(docker images -a --filter label=DAOS=true --quiet | tail -n 1)
 
-docker run --mount type=tmpfs,destination=/mnt/daos,tmpfs-mode=1777 $IMAGE ./utils/run_in_ga.sh
+docker run --name build-post --mount type=tmpfs,destination=/mnt/daos,tmpfs-mode=1777 $IMAGE ./utils/run_in_ga.sh
