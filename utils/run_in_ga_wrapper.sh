def03d1a4fa022fdda2d9edb0340887fd13ae827
Ashley Pittman <ashley.m.pittman@intel.com>
2021-05-07 14:53:45 +0100

    Add logging of CPU data.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/run_in_ga_wrapper.sh
+++ utils/run_in_ga_wrapper.sh
@@ -4,4 +4,5 @@ docker images -a --filter label=DAOS=true
 
 IMAGE=$(docker images -a --filter label=DAOS=true --quiet | tail -n 1)
 
-docker run --name build-post --mount type=tmpfs,destination=/mnt/daos,tmpfs-mode=1777 $IMAGE ./utils/run_in_ga.sh
+# 7Gb VM, 2g for docker, 2g for tmpfs.
+docker run --name build-post --memory=2g --mount type=tmpfs,destination=/mnt/daos,tmpfs-mode=1777,tmpfs-size=2097152 $IMAGE ./utils/run_in_ga.sh
