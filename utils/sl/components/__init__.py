d1fc62d1f86dae65d6f284a0d6fd7e1f1330e7aa
Ashley Pittman <ashley.m.pittman@intel.com>
2021-05-07 17:18:00 +0100

    Use westmere as it's the most recent for centos 7
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/sl/components/__init__.py
+++ utils/sl/components/__init__.py
@@ -298,14 +298,14 @@ def define_components(reqs):
                 retriever=retriever,
                 commands=['cd dpdk; '                            \
                           'git fetch; '                          \
-                          'git checkout origin/spdk-19.11.6',    \
+                          'git checkout origin/spdk-19.11.6',
                           './configure --prefix="$SPDK_PREFIX"'                \
                           ' --disable-tests --without-vhost --without-crypto'  \
                           ' --without-pmdk --without-vpp --without-rbd'        \
                           ' --with-rdma --with-shared'                         \
                           ' --without-iscsi-initiator --without-isal'          \
                           ' --without-vtune',
-                          'make $JOBS_OPT DPDK_CFLAGS=-march=x86-64',
+                          'make $JOBS_OPT DPDK_CFLAGS=-march=westmere',
                           'make install',
                           'cp dpdk/build/lib/* "$SPDK_PREFIX/lib"',
                           'mkdir -p "$SPDK_PREFIX/share/spdk"',
