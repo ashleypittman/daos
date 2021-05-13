b59b5040f1e70df29850e55c89637a02c9dd3f74
Ashley Pittman <ashley.m.pittman@intel.com>
2021-05-13 12:19:50 +0100

    Remove debug.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/run_in_ga_wrapper.sh
+++ utils/run_in_ga_wrapper.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-docker images -a --filter label=DAOS=true
-
-IMAGE=$(docker images -a --filter label=DAOS=true --quiet | tail -n 1)
+IMAGE=$(docker images -all --filter label=DAOS=true --quiet | tail -n 1)
 
 docker run --name build-post --mount type=tmpfs,destination=/mnt/daos,tmpfs-mode=1777 --env COMPILER $IMAGE ./utils/run_in_ga.sh
