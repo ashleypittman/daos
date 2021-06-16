8acf0b670d75847ad33a28dc4fc51b39251eb1b4
Ashley Pittman <ashley.m.pittman@intel.com>
2021-06-16 16:18:58 +0100

    Back out spdk change.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- site_scons/components/__init__.py
+++ site_scons/components/__init__.py
@@ -304,9 +304,10 @@ def define_components(reqs):
                 retriever=retriever,
                 commands=['cd dpdk; '                            \
                           'git fetch; '                          \
+                          'git checkout origin/spdk-19.11.6',    \
                           './configure --prefix="$SPDK_PREFIX"'                \
                           ' --disable-tests --without-vhost --without-crypto'  \
-                          ' --without-pmdk --without-rbd'                      \
+                          ' --without-pmdk --without-vpp --without-rbd'        \
                           ' --with-rdma --with-shared'                         \
                           ' --without-iscsi-initiator --without-isal'          \
                           ' --without-vtune', 'make $JOBS_OPT', 'make install',
