dd7c786404714fa663578db02f0dd0ded03fb84c
Ashley Pittman <ashley.m.pittman@intel.com>
2023-11-20 09:43:05 +0000

    Add a script to do the work.
    Required-githooks: true
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

---
+++ ci/run_shellcheck.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+# Find and run shellcheck on all shell scripts.  Previously the `file` command was used
+# to identify shell commands but that checks for a #! which some of our shell code does not
+# use so go purely on filename.
+find . \( -path ./.git -o -path ./venv -o -path ./build -o -path ./src/control/vendor \
+ -o -path ./install -o -path src/rdb/raft -type d  \) -prune -o -name "*.sh" -print | \
+ xargs -l1 shellcheck -f gcc
