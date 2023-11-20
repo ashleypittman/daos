f3442322b58563d8af5d20f56f89139e97fb8c09
Ashley Pittman <ashley.m.pittman@intel.com>
2023-11-20 09:56:24 +0000

    Make command exit on failure.
    Required-githooks: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- ci/run_shellcheck.sh
+++ ci/run_shellcheck.sh
@@ -6,5 +6,5 @@ set -e
 # to identify shell commands but that checks for a #! which some of our shell code does not
 # use so go purely on filename.
 find . \( -path ./.git -o -path ./venv -o -path ./build -o -path ./src/control/vendor \
- -o -path ./install -o -path src/rdb/raft -type d  \) -prune -o -name "*.sh" -print | \
- xargs -l1 shellcheck -f gcc
+ -o -path ./install -o -path src/rdb/raft -type d  \) -prune -o -name "*.sh" -exec \
+ shellcheck -f gcc \{\} \+
